4bc7b126824d45f1ca8c4f2dffae41c0000ecaa1
dp19 <darsha019@gmail.com>
2026-09-03 12:16:09 +0530

    Implement send function for payload transmission
    Refactor main function to use send helper for sending various payloads to the server.

--- client/client.go
+++ client/client.go
@@ -1,18 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"net"
+	"time"
 )
 
+func send(conn net.Conn, payload string, name string) {
+	fmt.Printf("Sending %s\n", name)
+
+	_, err := conn.Write([]byte(payload))
+	if err != nil {
+		fmt.Println("write error:", err)
+		return
+	}
+
+	time.Sleep(500 * time.Millisecond)
+}
+
 func main() {
-	conn, _ := net.Dial("tcp", "localhost:6379")
+	conn, err := net.Dial("tcp", "localhost:6379")
+	if err != nil {
+		panic(err)
+	}
 	defer conn.Close()
 
-	conn.Write([]byte(
-        "*2\r\n" +
-        ":1\r\n" +
-        "*2\r\n" +
-        "+OK\r\n" +
-        "$5\r\nhello\r\n",
-    ))
+	// + Simple String
+	send(
+		conn,
+		"+HELLO\r\n",
+		"Simple String",
+	)
+
+	// : Integer
+	send(
+		conn,
+		":42\r\n",
+		"Integer",
+	)
+
+	// $ Bulk String
+	send(
+		conn,
+		"$5\r\nhello\r\n",
+		"Bulk String",
+	)
+
+	// Empty Bulk String
+	send(
+		conn,
+		"$0\r\n\r\n",
+		"Empty Bulk String",
+	)
+
+	// * Array
+	send(
+		conn,
+		"*3\r\n:1\r\n:2\r\n:3\r\n",
+		"Array of Integers",
+	)
+
+	// Nested Array
+	send(
+		conn,
+		"*3\r\n:1\r\n$5\r\nhello\r\n*2\r\n:2\r\n:3\r\n",
+		"Nested Array",
+	)
+
+	fmt.Println("Done")
 }
